f5909c475ac13daba98b0236f8b46f840678e705
Mike <devprodct@gmail.com>
2024-01-04 10:23:44 +0200

    [next-auth] - add styling

--- app/(components)/Nav/Nav.tsx
+++ app/(components)/Nav/Nav.tsx
@@ -1,23 +1,13 @@
+import { Menu } from "lucide-react";
+
 export default function Nav() {
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-none">
-        <button className="btn btn-round btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-5 h-5 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </button>
-      </div>
+    <div className="navbar flex w-full justify-between">
+      <div className="cursor-pointer text-2xl">SaaSy Boilerplate</div>
+
+      <button className="btn btn-round btn-ghost">
+        <Menu />
+      </button>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Nav from "./(components)/Nav/Nav";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-between p-24">
-      <header>
+    <main className="flex min-h-screen justify-between p-16">
+      <header className="w-full">
         <Nav />
       </header>
     </main>

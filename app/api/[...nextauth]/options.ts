e92cfe7f92346e507f2239ea8a16c56f75dece32
Mike <devprodct@gmail.com>
2024-01-04 07:40:34 +0200

    [next-auth] - setup auth route, add .env types

---
+++ app/api/[...nextauth]/options.ts
@@ -0,0 +1,15 @@
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const options = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.NEXT_PUBLIC_GITHUB_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
+    }),
+  ],
+};
---
+++ environment.d.ts
@@ -0,0 +1,8 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_GITHUB_ID: string;
+    NEXT_PUBLIC_GITHUB_SECRET: string;
+    NEXT_PUBLIC_GOOGLE_ID: string;
+    NEXT_PUBLIC_GOOGLE_SECRET: string;
+  }
+}

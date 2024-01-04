22412f50acb3b8d4a2ceb42c9008311c908bdf51
Mike <devprodct@gmail.com>
2024-01-04 07:24:18 +0200

    [setup] - test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button className="btn btn-primary">Hello daisyUI!</button>
-    </main>
-  );
+  return <main className="flex min-h-screen justify-between p-24"></main>;
 }
